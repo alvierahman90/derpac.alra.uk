faa110c86a8f1d17eaa7d1569ad1061f5674ed17
derpac <derry.packer@gmail.com>
2021-05-07 19:07:15 +0100

    closes #1 move options into repo

--- buildandrun.sh
+++ buildandrun.sh
@@ -8,6 +8,5 @@ docker build -t "$NAME" "`dirname $0`"
 docker run -d -p 8000:80 \
 	--name "$NAME" \
 	--restart unless-stopped \
-	--mount type=bind,source=`pwd`/options,target=/usr/src/app/options,readonly \
 	--mount type=bind,source=`pwd`/votes.json,target=/usr/src/app/votes.json \
 	-t "$NAME" \
